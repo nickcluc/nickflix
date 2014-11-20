b8c27bbdf7505855001af4cc02ebd294fbd0a02f
nickcluc <nclucas01@gmail.com>
2014-11-20 09:20:56 -0500

    initial commit

---
+++ server.rb
@@ -0,0 +1,37 @@
+require 'pry'
+require 'csv'
+require 'sinatra'
+require 'sinatra/reloader'
+
+def read_movies
+  movies = []
+
+  CSV.foreach('movies.csv', headers: true) do |row|
+    movies << {
+      id: row['id'],
+      title: row['title'],
+      year: row['year'],
+      synopsis: row['synopsis'],
+      rating: row['rating'],
+      genre: row['genre'],
+      studio: row['studio']
+    }
+  end
+  movies
+end
+
+get '/movies' do
+  @movies = read_movies
+  # binding.pry
+  erb :collection
+end
+
+
+get '/movies/:movie_id' do
+  id = params[:movie_id]
+  @movie = read_movies.find do |movie|
+    movie[:id] == id
+  end
+  # binding.pry
+  erb :movie
+end
