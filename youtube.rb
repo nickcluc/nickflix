2312db4028b71f13f66787f91593c12af4aa7ff3
nickcluc <nclucas01@gmail.com>
2014-11-21 10:27:03 -0500

    updated

--- youtube.rb
+++ youtube.rb
@@ -1,52 +1,60 @@
 require 'rubygems'
+gem 'google-api-client', '>0.7'
 require 'google/api_client'
 require 'trollop'
+require 'pry'
+
+# Set DEVELOPER_KEY to the API key value from the APIs & auth > Credentials
+# tab of
+# {{ Google Cloud Console }} <{{ https://cloud.google.com/console }}>
+# Please ensure that you have enabled the YouTube Data API for your project.
+DEVELOPER_KEY = ENV["YOUTUBE_API_KEY"]
+YOUTUBE_API_SERVICE_NAME = 'youtube'
+YOUTUBE_API_VERSION = 'v3'
+# binding.pry
+
+def get_service
+  client = Google::APIClient.new(
+  :key => DEVELOPER_KEY,
+  :authorization => nil,
+  :application_name => 'NICKFLIX',
+  :application_version => '0.0.1'
+  )
+  youtube = client.discovered_api(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION)
+
+  return client, youtube
+end
 
 def get_url(movie)
-# Set developer_key to the "API key" value from the "Access" tab of the
-# Google Developers Console <https://cloud.google.com/console>
-# Please ensure that you have enabled the YouTube Data API for your project.
-  developer_key = "AIzaSyD7A3DCqncxjp-uvNS0dbU9ZNSOJEJL3uc"
-  youtube_api_service_name = "youtube"
-  youtube_api_version = "v3"
-
   opts = Trollop::options do
-    opt :q, movie, :type => String, :default => "#{movie} trailer"
-    opt :maxResults, 'Max results', :type => :int, :default => 25
+    opt :q, '#{movie}', :type => String, :default => movie + 'trailer'
+    opt :max_results, 'Max results', :type => :int, :default => 1
   end
 
-  client = Google::APIClient.new(:key => developer_key,
-  :authorization => nil)
-  youtube = client.discovered_api(youtube_api_service_name, youtube_api_version)
+  client, youtube = get_service
 
-  # Call the search.list method to retrieve results matching the specified
-  # query term.
-  opts[:part] = 'id,snippet'
-  search_response = client.execute!(
-  :api_method => youtube.search.list,
-  :parameters => opts
-  )
+  begin
+    # Call the search.list method to retrieve results matching the specified
+    # query term.
+    search_response = client.execute!(
+    :api_method => youtube.search.list,
+    :parameters => {
+      :part => 'snippet',
+      :q => opts[:q],
+      :maxResults => opts[:max_results]
+    }
+    )
 
-  videos = []
-  channels = []
-  playlists = []
-
+    videos = []
   # Add each result to the appropriate list, and then display the lists of
   # matching videos, channels, and playlists.
   search_response.data.items.each do |search_result|
     case search_result.id.kind
     when 'youtube#video'
-      videos.push("#{search_result.snippet.title} (#{search_result.id.videoId})")
-    when 'youtube#channel'
-      channels.push("#{search_result.snippet.title} (#{search_result.id.channelId})")
-    when 'youtube#playlist'
-      playlists.push("#{search_result.snippet.title} (#{search_result.id.playlistId})")
+      videos << "//youtube.com/embed/#{search_result.id.videoId}"
     end
   end
-
-  # puts opts
-  video_id = videos[0][(videos[0].length-12)...(videos[0].length-1)]
-
-  url = "//youtube.com/embed/#{video_id}"
+  url = videos[0]
   url
+  end
 end
